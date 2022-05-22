7f7da99725a3d403bc67b6915efa0006a344c9f8
iamhossain29 <mhossain.career@gmail.com>
2022-05-21 22:31:23 -0400

    Updated with Answers

--- birthdayparadox.py
+++ birthdayparadox.py
@@ -26,7 +26,7 @@ print('''Birthday Paradox''')
 MONTHS = ('Jan', 'Feb', 'Mar', 'APR', 'MAY', 'Jun', 'Jul', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
 
 while True:
-    print('How many birthdays shall I generate? (Max 100')
+    print('How many birthdays shall I generate? (Max 100)')
     response = input('> ')
     if response.isdecimal() and (0 < int(response) <= 100):
         numBDays = int(response)
@@ -60,7 +60,7 @@ input('Press ENTER to begin...')
 print('Let\'s run another 100,000 simulations.')
 simMatch = 0
 for i in range(100_000):
-    if i % 10_000 == 0:
+    if i % 10_00 == 0:
         print(i, 'simulations run....')
     birthdays = getBirthdays(numBDays)
     if getMatch(birthdays) is not None:
@@ -73,3 +73,11 @@ print('matching birthday in that group', simMatch, 'times. This means')
 print('that', numBDays, 'people have a', probability, '% chance of')
 print('having a matching birthday in their group.')
 print('That\'s probably more than you would think!')
+
+#########################################################################################
+
+# 1. How are birthdays represented in this program? YYYY/MM/DD
+# 2. How could you remove the maximum limit of 100 birthdays the program generates? By changing this  if response.isdecimal() and (0 < int(response) <= 100): to  if response.isdecimal() and (0 < int(response) <= 101):
+# 3. What error message do you get if you delete or comment out numBDays = int(response) on line 57? NameError: name 'numBDays' is not defined
+# 4. How can you make the program display full month names, such as 'January' instead of 'Jan'? It can be change here MONTHS = ('Jan', 'Feb', 'Mar', 'APR', 'MAY', 'Jun', 'Jul', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
+# 5. How could you make 'X simulations run...' appear every 1,000 simulations instead of every 10,000?  if i % 10_00 == 0:
